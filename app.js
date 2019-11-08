569cb9d9ef83d44ad06a17e87e00d4b7b88d5c4f
RMH <rory.hennessey@gmail.com>
2019-11-08 14:06:07 -0500

    implemented loading spinner. project complete.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults)
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+
+  e.preventDefault()
+})
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log('Calculating...')
   // UI Variables
   const amount = document.getElementById('amount');
@@ -24,15 +35,27 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2)
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+
+    //Show Results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
     showError('Please check your numbers')
   }
 
-  e.preventDefault()
 }
 
 // Show Error
 function showError(error) {
+  //Hide Results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide Loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
